b9c60dbdd7947fe837796826ef9665272cc6f39b
Thu Nguyen <thu_nguyen@gmx.de>
2022-09-16 12:59:16 +0200

    Pagination

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const prevButton = document.querySelector('[data-js="button-prev"]');
 const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
 
+const rooturl = 'https://rickandmortyapi.com/api/character';
 // States
 let maxPage = 1;
 let page = 1;
@@ -16,9 +17,11 @@ let searchQuery = '';
 
 cardContainer.innerHTML = '';
 
-fetchCharacters();
-function fetchCharacters() {
-  fetch('https://rickandmortyapi.com/api/character')
+fetchCharacters(rooturl);
+
+function fetchCharacters(url) {
+  console.log(url);
+  fetch(url)
     .then(response => response.json())
     .then(data => {
       data.results.forEach(result => {
@@ -37,3 +40,31 @@ function fetchCharacters() {
 function renderElement(element) {
   cardContainer.appendChild(element); // Append the Element to the page
 }
+
+function fetchMaxPage() {
+  return fetch(rooturl)
+    .then(response => response.json())
+    .then(data => data.info.pages);
+}
+
+nextButton.addEventListener('click', () => {
+  fetchMaxPage().then(maxPage => {
+    if (page > maxPage) return;
+    cardContainer.innerHTML = '';
+    ++page;
+    pagination.textContent = page + ' / ' + maxPage;
+    const url = rooturl + '?page=' + page;
+    fetchCharacters(url);
+  });
+});
+
+prevButton.addEventListener('click', () => {
+  fetchMaxPage().then(maxPage => {
+    if (page <= 1) return;
+    cardContainer.innerHTML = '';
+    --page;
+    pagination.textContent = page + ' / ' + maxPage;
+    const url = rooturl + '?page=' + page;
+    fetchCharacters(url);
+  });
+});
