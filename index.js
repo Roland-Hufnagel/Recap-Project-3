f52c16944ad3d71a8dc94c29856def71c7006323
Roland-Hufnagel <roland.hufnagel@gmx.de>
2022-09-16 15:07:16 +0200

    fix index.js

--- index.js
+++ index.js
@@ -15,27 +15,26 @@ let maxPage = 1;
 let page = 1;
 let searchQuery = '';
 fetchCharacters();
-function fetchCharacters() {
-  fetch('https://rickandmortyapi.com/api/character')
-    .then(response => response.json())
-    .then(data => {
-      //console.log(data);
-      const characters = data.results;
-      cardContainer.innerHTML = "";
-      characters.forEach(character => {
-        
-        
-        const profile = {
-          name: character.name, 
-          image: character.image,
-          type: character.type,
-          status: character.status,
-          occurences: character.episode.length,
-        }
-        
-        cardContainer.append(createCharacterCard(profile));
-      })
-      //.catch(console.error(error));
-      
-    });
+
+async function fetchCharacters() {
+  try{
+  const result = await fetch('https://rickandmortyapi.com/api/character');
+  const data = await result.json();
+
+  cardContainer.innerHTML = '';
+  data.results.forEach(character => {
+    const profile = {
+      name: character.name,
+      image: character.image,
+      type: character.type,
+      status: character.status,
+      occurences: character.episode.length,
+    };
+
+    cardContainer.append(createCharacterCard(profile));
+  });
+}catch(error){
+  console.error(error.message);
+}
+  
 }
