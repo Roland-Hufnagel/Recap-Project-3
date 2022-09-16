4ef94001302b319e92f9bc2b08e91d3a833c8273
Thu Nguyen <thu_nguyen@gmx.de>
2022-09-16 11:18:04 +0200

    fetch first 20 characters and display

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import {createCharacterCard} from './components/card/card.js';
 const cardContainer = document.querySelector('[data-js="card-container"]');
 const searchBarContainer = document.querySelector(
   '[data-js="search-bar-container"]',
@@ -12,3 +13,27 @@ const pagination = document.querySelector('[data-js="pagination"]');
 let maxPage = 1;
 let page = 1;
 let searchQuery = '';
+
+cardContainer.innerHTML = '';
+
+fetchCharacters();
+function fetchCharacters() {
+  fetch('https://rickandmortyapi.com/api/character')
+    .then(response => response.json())
+    .then(data => {
+      data.results.forEach(result => {
+        const profile = {
+          name: result.name,
+          img: result.image,
+          type: result.type,
+          status: result.status,
+          occurrences: result.episode.length,
+        };
+        renderElement(createCharacterCard(profile));
+      });
+    });
+}
+
+function renderElement(element) {
+  cardContainer.appendChild(element); // Append the Element to the page
+}
