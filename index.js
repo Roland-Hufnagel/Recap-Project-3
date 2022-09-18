147a00212b1d117a64065d4ca8ace590970cfca4
Thu Nguyen <thu_nguyen@gmx.de>
2022-09-18 13:08:56 +0200

    refractoring

--- index.js
+++ index.js
@@ -1,28 +1,26 @@
+import {createButton} from './components/nav-button/nav-button.js';
+import {createSearchBar} from './components/search-bar/search-bar.js';
+import {createPagination} from './components/nav-pagination/nav-pagination.js';
 import {createCharacterCard} from './components/card/card.js';
 const cardContainer = document.querySelector('[data-js="card-container"]');
 const searchBarContainer = document.querySelector(
   '[data-js="search-bar-container"]',
 );
-const searchBar = document.querySelector('[data-js="search-bar"]');
 const navigation = document.querySelector('[data-js="navigation"]');
-const prevButton = document.querySelector('[data-js="button-prev"]');
-const nextButton = document.querySelector('[data-js="button-next"]');
-const pagination = document.querySelector('[data-js="pagination"]');
+
+const main = document.querySelector('main');
 
 const rooturl = 'https://rickandmortyapi.com/api/character';
-// States
-let maxPage = 1;
-let page = 1;
-let searchQuery = '';
 
-cardContainer.innerHTML = '';
-
-fetchCharacters(rooturl + '?page=1');
+let frontpage = {maxPage: 1, page: 1, searchQuery: '', url: rooturl};
 
 function fetchCharacters(url) {
   fetch(url)
     .then(response => response.json())
     .then(data => {
+      cardContainer.innerHTML = '';
+      frontpage.maxPage = data.info.pages;
+      pagination.textContent = `${frontpage.page} / ${frontpage.maxPage}`;
       data.results.forEach(result => {
         const profile = {
           name: result.name,
@@ -31,53 +29,43 @@ function fetchCharacters(url) {
           status: result.status,
           occurrences: result.episode.length,
         };
-        renderElement(createCharacterCard(profile));
+
+        cardContainer.append(createCharacterCard(profile));
       });
-    });
+    })
+    .catch(error => console.error(error));
 }
 
-function renderElement(element) {
-  cardContainer.appendChild(element); // Append the Element to the page
-}
+fetchCharacters(rooturl);
 
-function fetchMaxPage(url) {
-  return fetch(url)
-    .then(response => response.json())
-    .then(data => data.info.pages);
-}
-
-fetchMaxPage(rooturl).then(maxPage => {
-  nextButton.addEventListener('click', () => {
-    if (page >= maxPage) return;
-    cardContainer.innerHTML = '';
-    ++page;
-    pagination.textContent = page + ' / ' + maxPage;
-    const url = rooturl + '?page=' + page;
+const prevButton = createButton(
+  frontpage,
+  url => {
     fetchCharacters(url);
+  },
+  'button--prev',
+  'button--prev',
+  'previous',
+  '',
+);
 
-    prevButton.addEventListener('click', () => {
-      if (page <= 1) return;
-      cardContainer.innerHTML = '';
-      --page;
-      pagination.textContent = page + ' / ' + maxPage;
-      const url = rooturl + '?page=' + page;
-      fetchCharacters(url);
-    });
-  });
-});
+const nextButton = createButton(
+  frontpage,
+  url => {
+    fetchCharacters(url);
+  },
+  'button--next',
+  'button--next',
+  'next',
+  '',
+);
+const pagination = createPagination(`${frontpage.page} / ${frontpage.maxPage}`);
 
-searchBar.addEventListener('submit', event => {
-  event.preventDefault();
-  const searchWhat = searchBar.elements.query.value;
-  const searchurl = rooturl + '?name=' + searchWhat;
-  console.log(searchurl);
-  fetchMaxPage(searchurl).then(maxsearchpage => {
-    console.log(maxsearchpage);
-    let fetchurl = '';
-    cardContainer.innerHTML = '';
-    for (let i = 1; i <= maxsearchpage; i++) {
-      fetchurl = rooturl + '?page=' + i + '&name=' + searchWhat;
-      fetchCharacters(fetchurl);
-    }
-  });
+navigation.append(prevButton);
+navigation.append(pagination);
+navigation.append(nextButton);
+
+const searchBar = createSearchBar(frontpage, url => {
+  fetchCharacters(url);
 });
+main.insertBefore(searchBar, cardContainer);
